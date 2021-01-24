3d768424a126ebd8da980530a493676db5b79438
BoghdanRohach <73877085+BoghdanRohach@users.noreply.github.com>
2021-01-25 00:48:32 +0200

    course testing

--- helpers/locators.py
+++ helpers/locators.py
@@ -1,6 +1,7 @@
 
 HOME_PAGE_BUTTON_DETAILS_BASE_QA='a[href="/services/base_qa"]'
 BASE_QA_PAGE_BUTTON_REGISTER = "body > div.service-content > div.service-info1 > div > a"
+
 HOME_PAGE_BUTTON_DETAILS_ENGLISH='a[href="/services/english_it"]'
 ENLISH_PAGE_BUTTON_TEXT = "#accordion > div:nth-child(12) > div.panel-heading > h4 > a > strong"
 HOME_PAGE_BUTTON_DETAILS_AUTOMATION_TESTING='a[href="/services/automation_testing"]'
--- tests/test_selenium.py
+++ tests/test_selenium.py
@@ -38,3 +38,31 @@ def test_get_title3(driver):
    driver.get("https://its.uz.ua/services/base_qa")
    assert 'its.uz.ua' in driver.title
 
+def test_click_english_it_button_on_homepage(driver):
+    get_homepage(driver)
+    # Перевірка кліку на кнопку Англійська для ІТ спеціалістів -> Детальніше
+    button_click(driver, HOME_PAGE_BUTTON_DETAILS_ENGLISH)
+    # Чекаємо на кнопку "Зареєструватись на іншій cторінці"
+    wait_for_element(driver, ENLISH_PAGE_BUTTON_TEXT)
+  # перевірити текст на новій сторінці
+    assert_text_on_page(driver, "English for IT Specialists")
+
+
+def test_click_automation_testing_button_on_homepage(driver):
+    get_homepage(driver)
+    # Перевірка кліку на кнопку Автоматизоване Тестування -> Детальніше
+    button_click(driver, HOME_PAGE_BUTTON_DETAILS_AUTOMATION_TESTING)
+    # Чекаємо на кнопку "Зареєструватись на іншій cторінці"
+    wait_for_element(driver, AUTOMATION_TESTING_PAGE_BUTTON_REGISTER)
+    # перевірити текст на новій сторінці
+    assert_text_on_page(driver, "Automation QA")
+
+
+def test_click_contact_button_on_homepage(driver):
+    get_homepage(driver)
+    # Перевірка кліку на кнопку Стань викладачем курсів -> Детальніше
+    button_click(driver, HOME_PAGE_BUTTON_DETAILS_TEACHER_COURSES)
+    # Чекаємо на кнопку "Зареєструватись на іншій cторінці"
+    wait_for_element(driver, TEACHER_COURSES_PAGE_BUTTON_REGISTER)
+    # перевірити текст на новій сторінці
+    assert_text_on_page(driver, "December 12, 2017")
