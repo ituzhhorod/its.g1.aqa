3acf3cd67999ce3152fcbb1ce0c2da631fa7a7af
BoghdanRohach <73877085+BoghdanRohach@users.noreply.github.com>
2021-01-27 00:27:10 +0200

    course testing

--- tests/test_english.py
+++ tests/test_english.py
@@ -38,9 +38,3 @@ def test_click_contact_button_on_homepage(driver):
     wait_for_element(driver, TEACHER_COURSES_PAGE_BUTTON_REGISTER)
     # перевірити текст на новій сторінці
     assert_text_on_page(driver, "December 12, 2017")
-HOME_PAGE_BUTTON_DETAILS_ENGLISH='a[href="/services/english_it"]'
-ENLISH_PAGE_BUTTON_TEXT = "#accordion > div:nth-child(12) > div.panel-heading > h4 > a > strong"
-HOME_PAGE_BUTTON_DETAILS_AUTOMATION_TESTING='a[href="/services/automation_testing"]'
-AUTOMATION_TESTING_PAGE_BUTTON_REGISTER = "body > div.service-content > div.container > div.row > div > h4"
-HOME_PAGE_BUTTON_DETAILS_TEACHER_COURSES='a[href="/contact"]'
-TEACHER_COURSES_PAGE_BUTTON_REGISTER = "body > div.about-bottom > div > div > blockquote:nth-child(13) > aside"
