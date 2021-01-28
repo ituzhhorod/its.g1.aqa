12b64f234a3c924b1014b6f2d5cd777fd2b66480
MykhailoTsibere <mtsibere.ukr@gmail com>
2021-01-29 01:21:19 +0200

    add test click contact button

--- helpers/locators.py
+++ helpers/locators.py
@@ -1,3 +1,10 @@
 
 HOME_PAGE_BUTTON_DETAILS_BASE_QA='a[href="/services/base_qa"]'
 BASE_QA_PAGE_BUTTON_REGISTER = "body > div.service-content > div.service-info1 > div > a"
+HOME_PAGE_BUTTON_AUTOMATION_TESTING='a[href="/services/automation testing"]'
+HOME_PAGE_BUTTON_ENGLISH_IT = 'a[href="/services/english_it"]'
+HOME_PAGE_BUTTON_CONTACT = 'a[href="/contact"]'
+CONTACT_PAGE_BUTTON_SEND = "#button"
+AUTOMATION_TESTING_PAGE_BUTTON_REGISTER = "#c_65u66 > div > button > span"
+ENGLISH_IT_PAGE_BUTTON_REGISTER = "#c_65u66 > div > button > span"
+HOME_PAGE_BUTTON_AUTOMATION_TESTING = "body > div.content > div:nth-child(1) > div > div:nth-child(4) > div > a"
